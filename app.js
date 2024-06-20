18d466edfe9aed1a26d77d1668f59d7762634193
akashmishrahaha <mishrakash1701@gmail.com>
2024-06-20 13:58:39 +0530

    Added files

---
+++ app.js
@@ -0,0 +1,21 @@
+// function toggleShowBox() {
+//   console.log("clicked");
+//   var showbox = document.getElementById("showbox");
+//   showbox.classList.toggle("visible");
+
+//   if (showbox.classList.contains("visible")) {
+//     // If showbox is visible, add sticky styles
+//     showbox.style.position = "fixed";
+//     showbox.style.top = "100px"; // Adjust this value as needed
+//   } else {
+//     // If showbox is not visible, reset position
+//     showbox.style.position = "static";
+//     showbox.style.top = "auto";
+//   }
+// }
+
+function toggleShowBox() {
+  console.log("clicked");
+  var showbox = document.getElementById("showbox");
+  showbox.classList.toggle("visible");
+}
